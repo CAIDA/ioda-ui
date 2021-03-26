6ab3f62e8b56be2fec96ba22aa1e2db3dcfd538b
Jon <jweber@caida.org>
2021-03-25 17:27:26 -0700

    all partner cards now linked out, new card added for dos and new layout adapted from v1

--- assets/js/Ioda/templates/Nav.js
+++ assets/js/Ioda/templates/Nav.js
@@ -57,28 +57,28 @@ class Nav extends Component {
                                 <span className="navigation__icon">&nbsp;</span>
                             </label>
                         </div>
-                        {/*<ul className="navigation__list">*/}
-                        {/*    <li className="navigation__item">*/}
-                        {/*        <Link to="/faq" className="navigation__link">*/}
-                        {/*            FAQ*/}
-                        {/*        </Link>*/}
-                        {/*    </li>*/}
-                        {/*    <li className="navigation__item">*/}
-                        {/*        <Link to="/docs" className="navigation__link">*/}
-                        {/*            Documentation*/}
-                        {/*        </Link>*/}
-                        {/*    </li>*/}
-                        {/*    <li className="navigation__item">*/}
-                        {/*        <Link to="/about" className="navigation__link">*/}
-                        {/*            About*/}
-                        {/*        </Link>*/}
-                        {/*    </li>*/}
-                        {/*    <li className="navigation__item">*/}
-                        {/*        <Link to="/help" className="navigation__link">*/}
-                        {/*            Help*/}
-                        {/*        </Link>*/}
-                        {/*    </li>*/}
-                        {/*</ul>*/}
+                        <ul className="navigation__list">
+                            <li className="navigation__item">
+                                <Link to="/dashboard" className="navigation__link">
+                                    Dashboard
+                                </Link>
+                            </li>
+                            {/*<li className="navigation__item">*/}
+                            {/*    <Link to="/docs" className="navigation__link">*/}
+                            {/*        Documentation*/}
+                            {/*    </Link>*/}
+                            {/*</li>*/}
+                            {/*<li className="navigation__item">*/}
+                            {/*    <Link to="/about" className="navigation__link">*/}
+                            {/*        About*/}
+                            {/*    </Link>*/}
+                            {/*</li>*/}
+                            {/*<li className="navigation__item">*/}
+                            {/*    <Link to="/help" className="navigation__link">*/}
+                            {/*        Help*/}
+                            {/*    </Link>*/}
+                            {/*</li>*/}
+                        </ul>
                     </nav>
                 </div>
             </div>
