ce2fb7718071517665b90de9d67f50536c4c633d
Jon <jweber@caida.org>
2021-07-30 12:49:17 -0700

    added ability to click on timestamp and it will copy to clipboard as well as present a message saying it has done so

--- assets/js/Ioda/components/timeStamp/TimeStamp.js
+++ assets/js/Ioda/components/timeStamp/TimeStamp.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
 import {convertSecondsToDateValues} from "../../utils";
+import T from "i18n-react";
 
 
 class TimeStamp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { fade: false }
+    }
+
+    copyTimestamp(timestamp) {
+        // copy to clipboard
+        navigator.clipboard.writeText(timestamp);
+
+        // trigger animation
+        this.setState({fade: true});
+    }
+
+    resetFadeState() {
+        this.setState({fade: false});
+    }
+
     render() {
         const timestamp = `${this.props.from.month} ${this.props.from.day}, ${this.props.from.year} ${this.props.from.hours}:${this.props.from.minutes}${this.props.from.meridian} — ${this.props.until.month} ${this.props.until.day}, ${this.props.until.year} ${this.props.until.hours}:${this.props.until.minutes}${this.props.until.meridian}`;
+        const fade = this.state.fade;
+        const copyToClipboardMessage = T.translate("timestamp.copyToClipboardMessage");
         return (
-            <React.Fragment>
-                <div className="timestamp">
+            <div className="timestamp" onClick={() => this.copyTimestamp(timestamp)}>
+                <div
+                    className={fade ? 'timestamp__message timestamp__fade' : 'timestamp__message'}
+                    onAnimationEnd={() => this.resetFadeState()}
+                    style={{top: event.target.offsetTop - 5, left: event.clientX}}
+                >
+                    {copyToClipboardMessage}
+                </div>
+                <div className="timestamp__text">
                     {timestamp}
                 </div>
-                {/*<div className="clipboard">*/}
-                {/*    <button onClick={() => {navigator.clipboard.writeText(timestamp)}}>*/}
-                {/*    Copy to Clipboard*/}
-                {/*    </button>*/}
-                {/*</div>*/}
-            </React.Fragment>
+            </div>
+
         );
     }
 }
