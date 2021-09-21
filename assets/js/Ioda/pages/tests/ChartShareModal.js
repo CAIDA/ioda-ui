d14f2828ab549958d5bb9aeec0f5ca8d8db43639
Jon <jweber@caida.org>
2021-09-21 16:32:26 -0700

    download button working for drawn on chart image

--- assets/js/Ioda/pages/tests/ChartShareModal.js
+++ assets/js/Ioda/pages/tests/ChartShareModal.js
@@ -2,14 +2,13 @@ import React, {PureComponent} from 'react';
 import PropTypes from "prop-types";
 import CanvasDraw from "react-canvas-draw";
 import {secondaryColor} from "../../utils";
+import html2canvas from "html2canvas";
 
 class ChartShareModal extends PureComponent {
     constructor(props) {
         super(props);
         this.state = {
             color: secondaryColor,
-            width: 400,
-            height: 400,
             brushRadius: 5,
             lazyRadius: 6,
             renderCanvas: false
@@ -23,8 +22,28 @@ class ChartShareModal extends PureComponent {
         }, 600);
     }
 
-    componentDidUpdate(prevProps) {
+    downloadFile() {
+        const input = document.getElementById('annotation');
+        html2canvas(input)
+            .then((canvas) => {
+                this.saveAs(canvas.toDataURL(), `${this.props.entityName}Chart.png`);
+            })
+    }
 
+    saveAs(uri, filename) {
+        let link = document.createElement('a');
+        if (typeof link.download === 'string') {
+            link.href = uri;
+            link.download = filename;
+            //Firefox requires the link to be in the body
+            document.body.appendChild(link);
+            //simulate click
+            link.click();
+            //remove the link when done
+            document.body.removeChild(link);
+        } else {
+            window.open(uri);
+        }
     }
 
     render() {
@@ -47,30 +66,26 @@ class ChartShareModal extends PureComponent {
                             <h3 style={{fontSize: '3rem', margin: '1rem 0'}}>Image standalone</h3>
                             <img src={this.props.imageFile} alt={this.props.entityName} style={{width: '100%', height: 'auto'}}/>
                             <h3 style={{fontSize: '3rem', margin: '1rem 0'}}>Image with drawing capability</h3>
-                            <button
-                                onClick={() => {
-                                    this.saveableCanvas.clear();
-                                }}
-                            >
+                            <button className="related__modal-button" style={{marginRight: '2rem'}} onClick={() => this.saveableCanvas.clear()}>
                                 Clear
                             </button>
-                            <button
-                                onClick={() => {
-                                    this.saveableCanvas.undo();
-                                }}
-                            >
+                            <button className="related__modal-button" style={{marginRight: '2rem'}} onClick={() => this.saveableCanvas.undo()}>
                                 Undo
                             </button>
+                            <button className="related__modal-button" onClick={() => this.downloadFile()}>
+                                Download
+                            </button>
                             {
-                                this.state.renderCanvas && <CanvasDraw
+                                this.state.renderCanvas && <div id="annotation"><CanvasDraw
                                     ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
                                     brushColor={this.state.color}
+                                    brushRadius={this.state.brushRadius}
+                                    lazyRadius={this.state.lazyRadius}
                                     imgSrc={this.props.imageFile}
                                     canvasWidth={this.headingRef.current.clientWidth}
                                     canvasHeight={(this.props.imageHeight * this.headingRef.current.clientWidth) / this.props.imageWidth}
-                                />
+                                /></div>
                             }
-                            <h3 style={{fontSize: '3rem', margin: '1rem 0'}}>Flattened Image with Drawing</h3>
                         </div>
                     </div>
                 </div>
