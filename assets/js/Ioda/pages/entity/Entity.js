8f95d34dfd01ae0f961213eb783956dc504a810f
JayWebz <jweber@caida.org>
2021-10-06 17:20:36 -0700

    update styles, remove console logs

--- assets/js/Ioda/pages/entity/Entity.js
+++ assets/js/Ioda/pages/entity/Entity.js
@@ -177,7 +177,6 @@ class Entity extends Component {
         this.maxHtsLimit = 150;
     }
     componentDidMount() {
-        console.log("update42");
         // Monitor screen width
         window.addEventListener("resize", this.resize.bind(this));
 
