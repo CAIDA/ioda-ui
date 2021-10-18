ca722cebf367db195851b2fb81de41a24b2a3434
JayWebz <jweber@caida.org>
2021-10-18 10:51:47 -0700

    fix download button styles

--- assets/js/Ioda/pages/entity/Entity.js
+++ assets/js/Ioda/pages/entity/Entity.js
@@ -177,7 +177,7 @@ class Entity extends Component {
         this.maxHtsLimit = 150;
     }
     componentDidMount() {
-        console.log("update4");
+        console.log("update5");
         // Monitor screen width
         window.addEventListener("resize", this.resize.bind(this));
 
